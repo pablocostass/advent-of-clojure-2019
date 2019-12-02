05ae68a660a79f40ee6351903c5a095a7d1ff12e
pablocostass <pablo.costass@udc.es>
2019-12-02 19:17:25 +0100

    Add day 2

--- src/advent_of_clojure/core.clj
+++ src/advent_of_clojure/core.clj
@@ -1,6 +1,7 @@
 (ns advent-of-clojure.core
   (:gen-class)
-  (:require [advent-of-clojure.week1.day1 :refer [day1]]))
+  (:require [advent-of-clojure.week1.day1 :refer [day1]]
+            [advent-of-clojure.week1.day2 :refer [day2]]))
 
 (defn -main [& args]
-  (day1))
+  (day1) (day2))
---
+++ src/advent_of_clojure/week1/day2.clj
@@ -0,0 +1,30 @@
+(ns advent-of-clojure.week1.day2
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn replace-value [input pos value]
+  (concat (take pos input) (cons value '()) (drop (+ pos 1) input)))
+
+(defn opcode-function [input offset operator]
+  (let [arg1 (nth input (nth input (+ offset 1)))
+        arg2 (nth input (nth input (+ offset 2)))
+        output-pos (nth input (+ offset 3))] 
+          (replace-value input output-pos (operator arg1 arg2))))
+
+(defn test1 [program offset]
+  (def opcode (nth program offset))
+  (cond
+    (= opcode 1) (test1 (opcode-function program offset +) (+ offset 4))
+    (= opcode 2) (test1 (opcode-function program offset *) (+ offset 4))
+    (= opcode 99) (first program)
+    :else (test1 program (+ offset 4))))
+
+(defn test2 [input]
+  (for [noun (range 100) verb (range 100)
+        :let [result (test1 (replace-value (replace-value input 1 noun) 2 verb) 0)]
+        :when (= 19690720 result)]
+    (+ (* 100 noun) verb)))
+
+(defn day2 []
+  (let [input (map read-string (str/split (slurp "resources/day2.txt") #","))]
+    (println "Day 2:\tTest1: " (test1 input 0) "\tTest2: " (first (test2 input)))))
