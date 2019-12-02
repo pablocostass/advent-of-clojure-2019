adf9a6c1bc79baafff64e987abf69383587eaae8
pablocostass <pablo.costass@udc.es>
2019-12-02 19:16:58 +0100

    Clean day1.clj

--- src/advent_of_clojure/week1/day1.clj
+++ src/advent_of_clojure/week1/day1.clj
@@ -3,26 +3,20 @@
   (:require [clojure.string :refer [split-lines]]))
 
 (defn calculate-fuel [input]
-  (def fun #(- (int (Math/floor (/ % 3))) 2))
-  (def result (reduce + (map fun input))) result)
+  (reduce + (map #(- (int (Math/floor (/ % 3))) 2) input)))
 
-(defn day1-test1 [input]
-  "Day 1 test 1"
-  (def result (calculate-fuel input)) result)
+(defn test1 [input]
+  (calculate-fuel input))
 
 (defn calculate-fuel-recursive [input acc]
-  (def fun #(- (int (Math/floor (/ % 3))) 2))
-  (def result (fun input))
+  (def result (#(- (int (Math/floor (quot % 3))) 2) input))
   (cond 
-  (<= result 0) acc
-  :else (calculate-fuel-recursive result (+ result acc))))
+    (<= result 0) acc
+    :else (recur result (+ result acc))))
 
-(defn day1-test2 [input]
-  "Day 1 test 2"
-  (def fun #(calculate-fuel-recursive % 0))
-  (def result (reduce + (map fun input))) result)
+(defn test2 [input]
+  (reduce + (map #(calculate-fuel-recursive % 0) input)))
 
 (defn day1 []
-  "https://adventofcode.com/2019/day/1"
   (let [input (map read-string (split-lines (slurp "resources/day1.txt")))]
-    (println (str "Day 1:\tTest1: " (day1-test1 input) "\tTest2: " (day1-test2 input)))))
+    (println (str "Day 1:\tTest1: " (test1 input) "\tTest2: " (test2 input)))))
