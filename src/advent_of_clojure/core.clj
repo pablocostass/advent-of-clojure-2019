ab9e24f1e77446b98ffad89b3df91cf2a76b1c00
pablocostass <pablo.costass@udc.es>
2019-12-08 12:20:27 +0100

    Add day 8 (part 1)

--- src/advent_of_clojure/core.clj
+++ src/advent_of_clojure/core.clj
@@ -3,8 +3,11 @@
   (:require [advent-of-clojure.week1.day1 :refer [day1]]
             [advent-of-clojure.week1.day2 :refer [day2]]
             [advent-of-clojure.week1.day4 :refer [day4]]
-            [advent-of-clojure.week1.day6 :refer [day6]]))
+            [advent-of-clojure.week1.day6 :refer [day6]]
+            [advent-of-clojure.week2.day8 :refer [day8]]
+  )
+)
 
 (defn -main [& args]
-  ;(day1) (day2) (day4)
-  (day6))
+  ;(day1) (day2) (day4) (day6) 
+  (day8))
---
+++ src/advent_of_clojure/week2/day8.clj
@@ -0,0 +1,37 @@
+(ns advent-of-clojure.week2.day8
+  (:gen-class)
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn check-layer [layer]
+  (assoc {} (->> layer
+       (filter #(= \0 %))
+       (count)) layer))
+
+(defn get-fewest-0-layer [layers]
+  (let [mapped-layers (map check-layer layers)]
+    (let [fewest-0-key (->> mapped-layers
+                            (map keys)
+                            (reduce #(into %1 %2) [])
+                            (sort)
+                            (first))]
+      (let [fewest-0-layer 
+            (->> mapped-layers
+                 (map #(get % fewest-0-key))
+                 (filter #(not= nil %))
+                 (first))]
+        fewest-0-layer))))
+
+(defn test1 [layers]
+  (let [layer (get-fewest-0-layer layers)]
+    (* 
+      (count (filter #(= \1 %) layer))
+      (count (filter #(= \2 %) layer)))))
+
+(defn day8 []
+  (let [input (->> "resources/day8.txt"
+                   (slurp)
+                   (split-lines)
+                   (first)
+                   (partition 150))]
+    ;(println (str "Day 1:\tTest1: " (test1 input) "\tTest2: " (test2 input)))
+    (println (str "Day 8:\tTest1: " (test1 input)))))
