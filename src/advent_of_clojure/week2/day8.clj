3844d959a4f6a472d4c4ce4a4f8c26e48fb705f8
pablocostass <pablo.costass@udc.es>
2019-12-08 17:41:55 +0100

    Add day 8 (part 2)

--- src/advent_of_clojure/week2/day8.clj
+++ src/advent_of_clojure/week2/day8.clj
@@ -1,13 +1,20 @@
 (ns advent-of-clojure.week2.day8
   (:gen-class)
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines join replace]]))
 
-(defn check-layer [layer]
+(defn check-layer
+  "Checks if a layer has the character `\0`
+  and if so, counts how many and returns
+  a map of {count, layer}"
+  [layer]
   (assoc {} (->> layer
        (filter #(= \0 %))
        (count)) layer))
 
-(defn get-fewest-0-layer [layers]
+(defn get-fewest-0-layer
+  "Gets from a map of {num-of-0s, layer}
+  the lowest number of 0s and returns its layer."
+  [layers]
   (let [mapped-layers (map check-layer layers)]
     (let [fewest-0-key (->> mapped-layers
                             (map keys)
@@ -21,17 +28,38 @@
                  (first))]
         fewest-0-layer))))
 
-(defn test1 [layers]
+(defn test1
+  [layers]
   (let [layer (get-fewest-0-layer layers)]
     (* 
       (count (filter #(= \1 %) layer))
       (count (filter #(= \2 %) layer)))))
+
+(defn stack-layers
+  "Stacks the layers of the image and removes
+  every transparent pixel (those with the 
+  character `\2`) and returns a joined string."
+  [layers]
+  (let [stacked-layers (partition 100 (apply interleave layers))]
+    (->> stacked-layers
+         (map (fn [pixel] (first (drop-while #(= % \2) pixel))))
+         (partition 25)
+         (map #(apply str %))
+         (join "\n"))))
+
+(defn test2
+  "Replaces the black pixels (the \0s) with spaces
+  and the white pixels (the \1s) with hashtags 
+  for ease of readibility."
+  [layers]
+  (let [stacked-layers (stack-layers layers)]
+    (replace
+      (replace stacked-layers #"1" "#") #"0" " ")))
 
 (defn day8 []
   (let [input (->> "resources/day8.txt"
                    (slurp)
                    (split-lines)
                    (first)
-                   (partition 150))]
-    ;(println (str "Day 1:\tTest1: " (test1 input) "\tTest2: " (test2 input)))
-    (println (str "Day 8:\tTest1: " (test1 input)))))
+                   (partition (* 25 6)))]
+    (println (str "Day 8:\tTest1: " (test1 input) "\tTest2: (down below)\n\n" (test2 input) "\n"))))
